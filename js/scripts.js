efcc886b69f4d3af5aaec41234deb07172454330
Meria Thomas and Skye Nguyen <meria@>
2018-08-08 10:28:55 -0700

    add initial files andinitial front-end logic

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+  $("form#input").submit(function(event) {
+    event.preventDefault();
+    var name = $("#name").val();
+    var app = $("#app").val();
+    var date = $("#date").val();
+    var timeS = $("#timeS").val();
+    var timeE = $("#timeE").val();
+  });
+});
